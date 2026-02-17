40cbc9bda86102d4127756552a405580dd46ca07
shailendra kumawat <kumawatsk2105@gmail.com>
2026-02-17 12:14:23 +0530

    make food and move the snake so that it eat food

--- project 2-snake game/script.js
+++ project 2-snake game/script.js
@@ -3,10 +3,12 @@ const blockHeight = 50;
 const blockWidth = 50;
 const cols = Math.floor(board.clientWidth / blockWidth);
 const rows = Math.floor(board.clientHeight / blockWidth);
-
+let intervalId = null;
 const blocks = [];
 
-let snake = [
+let food = {x:Math.floor(Math.random()*rows),y:Math.floor(Math.random()*cols)};
+
+const snake = [
     {
         x:1,
         y:2
@@ -26,29 +28,46 @@ for(let row = 0;row<rows;row++){
         const block = document.createElement('div');
         block.classList.add("block");
         board.appendChild(block);
-        // block.textContent= `${row}-${col}`
         blocks[`${row}-${col}`] = block;
     }  
 }
 
 function render(){
-    snake.forEach(segment =>{
-        blocks[`${segment.x}-${segment.y}`].classList.add("fill");
-    })
-}
-
-setInterval(() => {
     let head = null;
+
+    blocks[`${food.x}-${food.y}`].classList.add("food");
+
     if(direction ==='ArrowRight') head={x:snake[0].x,y:snake[0].y+1};
     else if(direction === 'ArrowLeft') head={x:snake[0].x,y:snake[0].y-1}
     else if(direction === 'ArrowDown') head={x:snake[0].x+1,y:snake[0].y}
     else if(direction === 'ArrowUp') head={x:snake[0].x-1,y:snake[0].y}
+
     
+    if(head.x<0 || head.y<0 || head.x>rows || head.y>cols){
+        clearInterval(intervalId);
+    }
+
+    if(head.x == food.x && head.y==food.y){
+        blocks[`${food.x}-${food.y}`].classList.remove("food");
+        food = {x:Math.floor(Math.random()*rows),y:Math.floor(Math.random()*cols)};
+        blocks[`${food.x}-${food.y}`].classList.add("food");
+        
+        snake.unshift(head);
+    }
+
     snake.forEach(segment =>{
         blocks[`${segment.x}-${segment.y}`].classList.remove("fill");
     })
+
     snake.unshift(head);
     snake.pop();
+
+    snake.forEach(segment =>{
+        blocks[`${segment.x}-${segment.y}`].classList.add("fill");  
+    })
+}
+
+intervalId = setInterval(() => {
     render();
 }, 300);
 
